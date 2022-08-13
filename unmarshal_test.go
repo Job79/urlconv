98b7aa8e71b0bca3fd0c6779c7a0e9cf8fe902e8
Job79 <job@plabble.org>
2022-08-13 11:36:31 +0200

    test: add unmarshal test

---
+++ unmarshal_test.go
@@ -0,0 +1,32 @@
+package urlconv
+
+import (
+	"net/url"
+	"testing"
+)
+
+type test struct {
+	String string   `url:"S"`
+	Int    int      `url:"I"`
+	Array  []string `url:"A"`
+}
+
+// TestUnmarshal tests whether the unmarshal function works correctly
+func TestUnmarshal(t *testing.T) {
+	values := url.Values{
+		"S": []string{"string"},
+		"I": []string{"1"},
+		"A": []string{"a", "b"},
+	}
+
+	var s test
+	Unmarshal(values, &s)
+
+	if s.String != "string" {
+		t.Errorf("string = %s, want string", s.String)
+	} else if s.Int != 1 {
+		t.Errorf("Int = %d, want 1", s.Int)
+	} else if len(s.Array) != 2 {
+		t.Errorf("Array = %v, want [a b]", s.Array)
+	}
+}
